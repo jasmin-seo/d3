1bb7fd7b0a072d0e15a8ab0f85a0203199e68e7c
yyc3 <sjandjhsister@gmail.com>
2021-11-22 12:14:10 +0900

    135

---
+++ src/app/line/line-component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'line-chart',
+  templateUrl: './line-template.html',
+  styleUrls: ['./line-component.scss']
+})
+
+export class lineComponent {
+
+}
